c36e7ffad1f12ebdd62a11717ad2e22d15b985ec
risingsun <243281121@qq.com>
2017-07-20 17:16:48 +0800

    完成 webpack 的第一个例子

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,19 +73,23 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_jquery__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_jquery__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__foo__ = __webpack_require__(5);
+
+
 
 
 function component() {
   var element = __WEBPACK_IMPORTED_MODULE_1_jquery___default()("<div></div>")
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = __WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hello', 'webpack'], ' ');
   element.html(__WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hello', 'webpack'], ' '));
 
   return element.get(0);
 }
 
 document.body.appendChild(component());
+console.log(__WEBPACK_IMPORTED_MODULE_2__foo__["a" /* default */]);
+console.log(Object(__WEBPACK_IMPORTED_MODULE_2__foo__["a" /* default */])())
 
 /***/ }),
 /* 1 */
@@ -27493,6 +27497,15 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony default export */ __webpack_exports__["a"] = (function(){
+   return '方方的教程'
+ });
 
 /***/ })
 /******/ ]);
