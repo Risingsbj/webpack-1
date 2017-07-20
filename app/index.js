c3b72c94077ed4c618f2922246ef6b93f80fa0aa
risingsun <243281121@qq.com>
2017-07-20 16:23:47 +0800

    完成 webpack 的第二个例子

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 import _ from "lodash";
+import j from "jquery";
 function component() {
-  var element = document.createElement('div');
+  var element = j("<div></div>")
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['Hello', 'webpack'], ' '));
 
-  return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
