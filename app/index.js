4d6e51b6458c69b2170053176d213962b8290277
risingsun <243281121@qq.com>
2017-07-20 16:29:29 +0800

    完成 webpack 的第二个例子

--- app/index.js
+++ app/index.js
@@ -4,7 +4,6 @@ function component() {
   var element = j("<div></div>")
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.html(_.join(['Hello', 'webpack'], ' '));
 
   return element.get(0);
