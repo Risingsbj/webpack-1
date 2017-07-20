179835beb240e7df8587fac69e9932ea06cfc042
risingsun <243281121@qq.com>
2017-07-20 20:24:42 +0800

    完成 webpack 的第一个例子

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import _ from "lodash";
 import j from "jquery";
 import foo from "./foo"
+import print from "./print"
 
 function component() {
   var element = j("<div></div>")
 
   // Lodash, currently included via a script, is required for this line to work
-  element.html(_.join(['Hello', 'webpack'], ' '));
+  element.html(_.join(['Hi', 'webpack'], ' '));
 
   return element.get(0);
 }
@@ -14,3 +15,4 @@ function component() {
 document.body.appendChild(component());
 console.log(foo);
 console.log(foo())
+console.log(print())
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
